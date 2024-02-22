64e58df72dd2dc578314df7829c2d8ef0d7447e5
Harrbys <130609346+Harrbys@users.noreply.github.com>
2024-02-22 08:37:05 -0500

    Add files via upload

---
+++ js/llaba.js
@@ -0,0 +1,157 @@
+function circulo(){
+    
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('circulo')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('circulo')
+    figuras.classList.toggle("circulo");
+    alert("Se va a descontrolar");
+}
+function rectangulo(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('rectangulo')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('rectangulo')
+    figuras.classList.toggle("rectangulo");
+    alert("Se va a descontrolar");
+}
+function triangulo(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('triangulo')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('triangulo')
+    figuras.classList.toggle("triangulo");
+    alert("Se va a descontrolar");
+}
+function estrella(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('estrella')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('estrella')
+    figuras.classList.toggle("estrella");
+    alert("Se va a descontrolar");
+}
+function moverrigth(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('moverrigth')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('moverrigth')
+    figuras.classList.toggle("moverrigth");
+    alert("Se va a descontrolar");
+}
+function moverleft(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('moverleft')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('moverleft')
+    figuras.classList.toggle("moverleft");
+    alert("Se va a descontrolar");
+}
+function movertop(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('movertop')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('movertop')
+    figuras.classList.toggle("movertop");
+    alert("Se va a descontrolar");
+}
+function moverbotton(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('moverbotton')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('moverbotton')
+    figuras.classList.toggle("moverbotton");
+    alert("Se va a descontrolar");
+}
+function fondo(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('fondo')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('fondo')
+    figuras.classList.toggle("fondo");
+    $var1=prompt("ingrese")
+    var $var1 = $var1;
+    figuras.style.background = $var1;
+    alert("Se va a descontrolar");
+}
+function degradado(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('rectangulo')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('rectangulo')
+    figuras.classList.toggle("degradado");
+    alert("Se va a descontrolar");
+}
+function diagonal(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('degradado')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('degradado')
+    figuras.classList.toggle("diagonal");
+    alert("Se va a descontrolar");
+}
+function luna(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('luna')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('luna')
+    figuras.classList.toggle("luna");
+    alert("Se va a descontrolar");
+}
+function PanelL(){
+    let Panel = document.getElementById("Panel");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('Panel Lateral')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('Panel Lateral')
+    Panel.classList.toggle("activeL");
+    alert("Se va a descontrolar");
+}
+function PanelR(){
+    let Panel = document.getElementById("Panel");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('Panel Lateral')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('Panel Lateral')
+    Panel.classList.toggle("activeR");
+    alert("Se va a descontrolar");
+}
+function PanelS(){
+    let PanelS = document.getElementById("Panel-superior");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('Panel Superior')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('Panel Superior')
+    PanelS.classList.toggle("active");
+    alert("Se va a descontrolar");
+}
+function fondoimg(){
+    let figuras = document.getElementById("figuras");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('Panel fondoimg')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('Panel fondoimg')
+    figuras.classList.toggle("fondoimg");
+    alert("Se va a descontrolar");
+}
+function fondoimg1(){
+    let full = document.getElementById("full");
+    let titulo = document.getElementById("titulo");
+    titulo.innerText=('Panel fondoimg')
+    let titulo1 = document.getElementById("titulo1");
+    titulo1.innerText=('Panel fondoimg')
+    full.classList.toggle("fondoimg1");
+    alert("Se va a descontrolar");
+}
